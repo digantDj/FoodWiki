88d2d9b0150331fecf4631492c6f0e1be46f743d
deepikak02 <dkrish15@asu.edu>
2015-12-02 15:12:33 -0700

    Integrated Apache Jena with web application
    Integrated Apache Jena with web application

--- FoodWiki/src/com/foodwiki/pkg/Home.java
+++ FoodWiki/src/com/foodwiki/pkg/Home.java
@@ -2,9 +2,10 @@ package com.foodwiki.pkg;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Home
  */
-@WebServlet(description = "The default Home Page for FoodWIki", urlPatterns = { "/Home" })
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -29,9 +29,23 @@ public class Home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//PrintWriter out = response.getWriter().append("Served at: ").append(request.getContextPath());
 		String element_1_value = request.getParameter("search") ;
+		ServletContext context = request.getSession().getServletContext();
 		PrintWriter out = response.getWriter();
+        Business business = new Business();
+
+       // business.populateFood();
+
+       /* Map result = null;
+		try {
+			result = business.getResult(element_1_value);
+			context.setAttribute("search", element_1_value);
+			context.setAttribute("result",result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+ 		response.sendRedirect("/data.jsp");*/       
 		String output = element_1_value;
 		out.print(output);
 		
