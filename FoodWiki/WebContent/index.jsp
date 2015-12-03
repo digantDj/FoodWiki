1a9a243d378a3467700c1d590a6ab1e1d6051b34
Digant Jagtap <digantjagtap@utoadmiacintosh.mobile.asu.edu>
2015-12-02 18:11:10 -0700

    UI for homepage and Search implementation

--- FoodWiki/WebContent/index.jsp
+++ FoodWiki/WebContent/index.jsp
@@ -5,17 +5,65 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
-<style>
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/foodwiki.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  		<script>
+  $(function() {
+	  var foodNamesString;
+	  $.ajax({
+          url : 'GetFoodNames',
+          async: false,
+          success : function(responseText) {
+              foodNamesString = responseText;
+          }
+      });
+	  
+    var availableTags = foodNamesString.split(',');
+    console.log(availableTags);
+    //availableTags = ["cars","scooter"];
+    $("#search").autocomplete({
+      source: availableTags
+    });
 
-</style>
+  });
+  </script>
+  
 </head>
-<body>
-<div id="foodWikiBody">	
-	<h1>Welcome to FoodWiki</h1>
-	<form action="Home" method="post">
-		<input type="text" id="search"  name="search">
-		<input type="submit" value="Submit"/>
+<body>	
+	<!-- Header -->
+			<header id="header">
+				<h1>Welcome to Food Wiki</h1>
+				<p>A semantic food wiki implementation.</p>
+			</header>
+			
+	<!-- Search Form -->		
+	<div class="ui-widget">
+	<form id="foodwiki-form" action="Home" method="post">
+		<input type="text" id="search"  name="search" placeholder="Search for a food product or dish">
+		<input type="submit" value="SEARCH"/>
 	</form>
-</div>
+	</div>
+	<!-- Footer -->
+			<footer id="footer">
+				<ul class="icons">
+					<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+					<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
+					<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+				</ul>
+				<ul class="copyright">
+					<li>&copy; FoodWiki.</li><li>Web Semantics Project 2015 - SER 594 ( Team 14 )</li>
+				</ul>
+			</footer>
+
+	<!-- Scripts -->
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
 </body>
 </html>
--- FoodWiki/src/com/foodwiki/pkg/GetFoodNames.java
+++ FoodWiki/src/com/foodwiki/pkg/GetFoodNames.java
@@ -3,8 +3,6 @@ package com.foodwiki.pkg;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Home
+ * Servlet implementation class GetFoodNames
  */
-@WebServlet(description = "The default Home Page for FoodWIki", urlPatterns = { "/Home" })
-public class Home extends HttpServlet {
+@WebServlet("/GetFoodNames")
+public class GetFoodNames extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Home() {
+    public GetFoodNames() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,25 +29,28 @@ public class Home extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//PrintWriter out = response.getWriter().append("Served at: ").append(request.getContextPath());
-		String element_1_value = request.getParameter("search") ;
-		PrintWriter out = response.getWriter();
-		String aboutString ="";
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		 
 		BufferedReader br = new BufferedReader(new FileReader(GetFoodNames.class.getClassLoader().getResource("foodnames.txt").getPath()));
 		try {
+		    StringBuilder sb = new StringBuilder();
 		    String line = br.readLine();
-		    while (line != null) {	
+
+		    while (line != null) {
 		    	
-		        if(line.split(" rdf:about=")[0].equals(element_1_value)){
-		        	aboutString = line.split(" rdf:about=")[1];
-		        	break;
+		        sb.append(line.split(" rdf:about=")[0]);
+		        line = br.readLine();
+		        if(line != null){
+		        	sb.append(",");
 		        }
-		        line = br.readLine();        
-		    }		    
+		        //sb.append(System.lineSeparator());        
+		    }
+		    String foodNames = sb.toString();
+		    response.setContentType("text/plain");
+	        response.getWriter().write(foodNames);
 		} finally {
 		    br.close();
 		}	
-		out.print("<b>"+aboutString+"</b>");
 	}
 
 	/**
@@ -58,7 +59,6 @@ public class Home extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
 	}
 
 }
